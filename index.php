8d940602de921b4aeae5a5b1babd0ccc4fda5fb4
f1703 <franco170391@gmail.com>
2018-03-08 11:04:02 -0300

    metaPropeller

--- index.php
+++ index.php
@@ -4,16 +4,13 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="propeller" content="a89615c9c4095cfe315d10dbf31bc347">
     <title>Sitio</title>
 
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-    <style media="screen">
-      .content{
-        margin-bottom: 40px;
-      }
-    </style>
+
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
